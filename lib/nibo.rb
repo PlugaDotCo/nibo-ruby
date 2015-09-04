3263ab861cb794ddcde9a1095c4229a2389ed9d1
Arthur Granado <agranado@smartcoin.com.br>
2015-09-04 15:46:31 -0300

    Create generate_hash for authentication for Nibo API.

--- lib/nibo.rb
+++ lib/nibo.rb
@@ -1,22 +1,44 @@
 require "json"
+require "base64"
+require "rest-client"
+require "active_support/all"
 
 require "nibo/version"
 
 require "nibo/object"
+require "nibo/api_resource"
+require "nibo/api_resource/retrieve"
 
 module Nibo
-  def self.api_key(api_key)
+  @@api_key
+  @@api_secret
+  @@user
 
+  def self.api_key=(api_key)
+    @@api_key = api_key
   end
 
-  def self.api_secret(api_secret)
-
+  def self.api_key
+    @@api_key
   end
 
-  def self.user(user)
-
+  def self.api_secret=(api_secret)
+    @@api_secret = api_secret
   end
 
+  def self.user=(user)
+    @@user = user
+  end
+
+  def self.user
+    @@user
+  end
+
+  def self.generate_hash(time_stamp)
+    Base64.encode64(OpenSSL::HMAC.digest('sha1',
+                                         Base64.decode64(Base64.encode64(@@api_secret).gsub("\n", '')),
+                                         "#{@@api_key}|#{time_stamp}|#{@@user}|#{@@api_key}")).chomp
+  end
 
   class Account
     def self.retrieve(account_id)
--- spec/nibo_spec.rb
+++ spec/nibo_spec.rb
@@ -4,4 +4,22 @@ describe Nibo do
   it 'has a version number' do
     expect(Nibo::VERSION).not_to be nil
   end
+
+  it 'should generate hash for authentication' do
+    user = 'agranado@smartcoin.com.br'
+    api_key = '47d9290a1c4c46efaaf0173369da2d8c'
+    api_secret = 'f13dd0cfd2a945b384e43cb0150904e3e756200308374982a71ee2e496fbb51cf4f670ae729d48ffb2284d1720c4d66a'
+    time_stamp =  Time.now.utc.strftime('%Y%m%d%H%M')
+    Nibo.api_key = api_key
+    Nibo.api_secret = api_secret
+    Nibo.user = user
+
+    key = Base64.decode64(Base64.encode64(api_secret).gsub("\n", ''))
+    #Create signature
+    signature = "#{api_key}|#{time_stamp}|#{user}|#{api_key}"
+    #HMCA (SHA-1) the signature
+    hash = Base64.encode64(OpenSSL::HMAC.digest('sha1', key, signature)).chomp
+
+    expect(Nibo.generate_hash(time_stamp)).to eq(hash)
+  end
 end
