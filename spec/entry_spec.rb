d797d397453828b42fac142a4ea5cdc9e7e4413c
Arthur Granado <agranado@smartcoin.com.br>
2015-10-31 19:39:38 -0200

    TestFix: Try to throw exception when miss some required param when create a new Entry.

--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
-describe Nibo::Entry do
+describe Nibo::Entry, wip: true do
   before(:each) do
     Nibo.api_key = '47d9290a1c4c46efaaf0173369da2d8c'
     Nibo.api_secret = 'f13dd0cfd2a945b384e43cb0150904e3e756200308374982a71ee2e496fbb51cf4f670ae729d48ffb2284d1720c4d66a'
     Nibo.user = 'test@test.com'
   end
 
-  it 'should create a new Entry in Nibo', wip: true do
+  it 'should create a new Entry in Nibo' do
     date = Time.now.utc.strftime('%Y-%m-%d')
     params = {
               AccountId: '991f6cc0-0234-4cc2-af5b-6099c912a54e',
@@ -28,5 +28,18 @@ describe Nibo::Entry do
     expect(entry.Date).to eq("#{date}T00:00:00")
     expect(entry.IsTransfer).to be_falsey
 
+  end
+
+  it 'should failt to create a new Entry because there is no Entity Id' do
+    date = Time.now.utc.strftime('%Y-%m-%d')
+        params = {
+                  AccountId: '991f6cc0-0234-4cc2-af5b-6099c912a54e',
+                  Value: 120,
+                  Description: 'Description test',
+                  Date: "#{date}T00:00:00",
+                  IsTransfer: false
+                }
+
+    expect{ Nibo::Entry.create(params)}.to raise_error
   end
 end
