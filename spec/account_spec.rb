704a7184438c9e6ed8f12809facb1f76a1ce7521
Arthur Granado <agranado@smartcoin.com.br>
2015-10-31 19:38:35 -0200

    TestFix: The order of strftime was wrong.

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -9,7 +9,7 @@ describe Nibo::Account do
     Nibo.api_key = '47d9290a1c4c46efaaf0173369da2d8c'
     Nibo.api_secret = 'f13dd0cfd2a945b384e43cb0150904e3e756200308374982a71ee2e496fbb51cf4f670ae729d48ffb2284d1720c4d66a'
     Nibo.user = 'test@test.com'
-    time_stamp =  Time.now.utc.strftime('%d/%m/%Y')
+    time_stamp =  Time.now.utc.strftime('%m/%d/%Y')
 
     params = {Description: 'Banco do Brasil',
               Balance: 100.0,
