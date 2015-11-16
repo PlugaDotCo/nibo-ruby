8ffc961e0dee2ad3b068b064d4ec833ea8efb028
Arthur Granado <agranado2k@gmail.com>
2015-11-16 19:44:15 -0200

    Why: Adapt the spec to new Account API
    * Nibo change the Balance Date format
    * Nibo crashed API for list all Accounts
    
    This change addresses the need by:
    
    * Change some tests to accept the new balance date format
    * Nibo fix the API problem.

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -14,7 +14,7 @@ describe Nibo::Account, vcr: {match_requests_on: [:host]} do
 
     params = {Description: 'Banco do Brasil',
               Balance: 100.0,
-              BalanceDate: time_stamp.strftime('%d/%m/%Y')}
+              BalanceDate: time_stamp.strftime('%m/%d/%Y')}
 
     account = Nibo::Account.create(params)
 
@@ -22,37 +22,52 @@ describe Nibo::Account, vcr: {match_requests_on: [:host]} do
     expect(account.AccountId).to_not be_nil
     expect(account.Description).to eq('Banco do Brasil')
     expect(account.Balance).to eq(100.0)
-    expect(account.BalanceDate).to eq(time_stamp.strftime('%Y-%d-%mT00:00:00'))
+    expect(account.BalanceDate).to eq(time_stamp.strftime('%Y-%m-%dT00:00:00'))
   end
 
-  it 'should get account' do
-    account_id = '10968472-9926-453b-a0b1-fc74f029d512'
-    Nibo.api_key = '47d9290a1c4c46efaaf0173369da2d8c'
-    Nibo.api_secret = 'f13dd0cfd2a945b384e43cb0150904e3e756200308374982a71ee2e496fbb51cf4f670ae729d48ffb2284d1720c4d66a'
-    Nibo.user = 'test@test.com'
+  context 'Account alread exist' do
+    before(:each) do
+      Nibo::Account.list.each do |account|
+        Nibo::Account.delete(account.AccountId)
+      end
 
-    result = {"OrganizationId" => "5d2b63a1-29ed-4204-b713-708c4afc0238","AccountId" => "10968472-9926-453b-a0b1-fc74f029d512","Description" => "Banco do Brasil","Balance" => 0.0,"BalanceDate" => "0001-01-01T00:00:00"}.to_json
-    allow(RestClient).to receive(:get).and_return(result)
-    account = Nibo::Account.retrieve(account_id)
+      params = {Description: 'Banco do Brasil',
+              Balance: 100.0,
+              BalanceDate: time_stamp.strftime('%m/%d/%Y')}
 
-    expect(account.OrganizationId).to eq('5d2b63a1-29ed-4204-b713-708c4afc0238')
-    expect(account.AccountId).to eq('10968472-9926-453b-a0b1-fc74f029d512')
-    expect(account.Description).to eq('Banco do Brasil')
-    expect(account.Balance).to eq(0.0)
-    expect(account.BalanceDate).to eq('0001-01-01T00:00:00')
-  end
+      Nibo::Account.create(params)
+      @account = Nibo::Account.create(params)
+    end
 
-  it 'should list all accounts' do
-    accounts = Nibo::Account.list
+    it 'should get account' do
+      account_id = @account.AccountId
+      Nibo.api_key = '47d9290a1c4c46efaaf0173369da2d8c'
+      Nibo.api_secret = 'f13dd0cfd2a945b384e43cb0150904e3e756200308374982a71ee2e496fbb51cf4f670ae729d48ffb2284d1720c4d66a'
+      Nibo.user = 'test@test.com'
 
-    expect(accounts.size).to eq(2)
-  end
+      result = {"OrganizationId" => "5d2b63a1-29ed-4204-b713-708c4afc0238","AccountId" => @account.AccountId,"Description" => "Banco do Brasil","Balance" => 0.0,"BalanceDate" => "0001-01-01T00:00:00"}.to_json
+      allow(RestClient).to receive(:get).and_return(result)
+      account = Nibo::Account.retrieve(account_id)
 
-  it 'should delete an account from Nibo API' do
-    account_id = '10968472-9926-453b-a0b1-fc74f029d512'
+      expect(account.OrganizationId).to eq('5d2b63a1-29ed-4204-b713-708c4afc0238')
+      expect(account.AccountId).to eq(account.AccountId)
+      expect(account.Description).to eq('Banco do Brasil')
+      expect(account.Balance).to eq(0.0)
+      expect(account.BalanceDate).to eq('0001-01-01T00:00:00')
+    end
 
-    account = Nibo::Account.delete(account_id)
+    it 'should list all accounts' do
+      accounts = Nibo::Account.list
 
-    expect(account).to eq('deleted')
+      expect(accounts.size).to eq(2)
+    end
+
+    it 'should delete an account from Nibo API' do
+      account_id = @account.AccountId
+
+      account = Nibo::Account.delete(account_id)
+
+      expect(account).to eq('deleted')
+    end
   end
 end
